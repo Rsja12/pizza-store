1f826974d46d79b84e08fbd35c87dfcf706a08a0
Rsja12 <asocarras10@gmail.com>
2020-08-08 15:14:44 -0400

    add snapshot test to Homepage component

---
+++ client/src/components/__tests__/Homepage.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Homepage from '../Homepage';
+import { shallow } from 'enzyme';
+
+it ('renders Homepage component', () => {
+    expect(shallow(<Homepage />)).toMatchSnapshot();
+});
