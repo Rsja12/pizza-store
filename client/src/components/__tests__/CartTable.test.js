71cf34893a1b38e610220f49d7862624e8d18fe9
Rsja12 <asocarras10@gmail.com>
2020-08-09 15:41:04 -0400

    add unit test for CartTable component

---
+++ client/src/components/__tests__/CartTable.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CartTable from '../CartTable';
+
+it ('renders CartTable component', () => {
+    const mockCart = [
+        {
+            id: 1,
+            pizza: 'pepperoni',
+            quantity: 2
+        }
+    ];
+
+    expect(shallow(<CartTable cart={mockCart} />)).toMatchSnapshot();
+});
