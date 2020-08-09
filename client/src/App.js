58982db1b3ea8eb3e793f41be8b98a627d26042e
Rsja12 <asocarras10@gmail.com>
2020-08-09 15:34:34 -0400

    remove Success component and Success Route

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Homepage from './components/Homepage'
 import Menu from './components/Menu'
 import Cart from './components/Cart'
 import Navbar from './components/Navbar'
-import Success from './components/Success'
 import './styles/App.css'
 
 const App = () => {
@@ -16,7 +15,6 @@ const App = () => {
                 <Route exact path="/" component={Homepage} />
                 <Route exact path="/menu" component={Menu} />
                 <Route exact path="/cart" component={Cart} />
-                <Route exact path="/success" component={Success} />
             </div>
         </BrowserRouter>
     )
