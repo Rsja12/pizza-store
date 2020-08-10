ea60bdda3c9c06473fb30c10c431c9d073ca12e3
Rsja12 <asocarras10@gmail.com>
2020-08-10 17:04:54 -0400

    add unit tests for CartList component

--- client/src/components/CartList.js
+++ client/src/components/CartList.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { increaseQuantity, decreaseQuantity, removeFromCart } from '../redux/actions'
 import '../styles/Cart.css'
 
-const CartList = props => {
+export const CartList = props => {
 
     const { name, quantity } = props.pizza 
     return (
@@ -12,11 +12,11 @@ const CartList = props => {
             <td className='font-weight-bold'>{name}</td>
             <td>
                 <span 
-                    className='quantity-btn'
+                    className='quantity-btn decrease'
                     onClick={() => props.decreaseQuantity(props.pizza)}>&#10134;</span>
                 <span className='quantity'>{quantity}</span>
                 <span 
-                    className='quantity-btn'
+                    className='quantity-btn increase'
                     onClick={() => props.increaseQuantity(props.pizza)}>&#10133;</span>
             </td>
             <td>
---
+++ client/src/components/__tests__/CartList.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { CartList } from '../CartList';
+
+
+let wrapper;
+let mockProps;
+
+beforeEach(() => {
+    mockProps = {
+        pizza: {
+            id: 1,
+            name: 'test',
+            quantity: 3
+        },
+        decreaseQuantity: jest.fn(),
+        increaseQuantity: jest.fn(),
+        removeFromCart: jest.fn()
+    };
+    
+    wrapper = shallow(<CartList {...mockProps} />)
+})
+
+it ('renders without crashing', () => {
+    expect(wrapper).toMatchSnapshot();
+});
+
+it ('calls functions on icon click', () => {
+    wrapper.find('.quantity-btn.decrease').simulate('click');
+    expect(mockProps.decreaseQuantity).toHaveBeenCalledTimes(1);
+
+    wrapper.find('.quantity-btn.increase').simulate('click');
+    expect(mockProps.increaseQuantity).toHaveBeenCalledTimes(1);
+
+    wrapper.find('.remove-btn').simulate('click');
+    expect(mockProps.removeFromCart).toHaveBeenCalledTimes(1);
+});
