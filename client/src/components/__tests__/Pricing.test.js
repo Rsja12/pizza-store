9a5a7a488b8fe22f5b903ea78535322dc90a5df5
Rsja12 <asocarras10@gmail.com>
2020-08-11 15:10:28 -0400

    add unit tests to Pricing component

--- client/src/components/__tests__/Pricing.test.js
+++ client/src/components/__tests__/Pricing.test.js
@@ -3,3 +3,15 @@ import { shallow } from 'enzyme';
 
 import { Pricing } from '../Pricing';
 
+
+it ('renders without crashing with empty cart', () => {
+    const mockProps = [];
+
+    expect(shallow(<Pricing cart={mockProps} />))
+});
+
+it ('renders without crashing with non-empty cart', () => {
+    const mockProps = [{}];
+
+    expect(shallow(<Pricing cart={mockProps} />))
+});
