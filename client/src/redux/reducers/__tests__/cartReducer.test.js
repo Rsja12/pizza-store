53449d0c15a168866e15e157341ca9c47f7586be
Rsja12 <asocarras10@gmail.com>
2020-08-13 20:08:45 -0400

    add more unit tests to cartReducer

--- client/src/redux/reducers/__tests__/cartReducer.test.js
+++ client/src/redux/reducers/__tests__/cartReducer.test.js
@@ -1,4 +1,5 @@
 import { cartReducer } from '../cartReducer';
+import * as actions from '../../actions'
 
 describe('cart', () => {
 
@@ -18,18 +19,47 @@ describe('cart', () => {
         }))
         .toEqual({
             ...initialState,
-            '1': {id: 1, name: 'pepperoni', quantity: 1}
+            1: {id: 1, name: 'pepperoni', quantity: 1}
         })
     });
 
     it ('handles REMOVE_FROM_CART', () => {
+        expect(cartReducer(initialState, actions.removeFromCart))
+        .toEqual({...initialState})
+    });
+
+    it ('handles INC_QTY', () => {
         expect(cartReducer(initialState, {
-            type: 'REMOVE_FROM_CART',
+            type: 'INC_QTY',
             payload: {
                 id: 1,
-                name: 'pepperoni'
+                name: 'pepperoni', 
+                quantity: 1
             }
         }))
+        .toEqual({
+            ...initialState,
+            1: {id: 1, name: 'pepperoni', quantity: 2}
+        })
+    });
+
+    it ('handles DEC_QTY', () => {
+        expect(cartReducer(initialState, {
+            type: 'DEC_QTY',
+            payload: {
+                id: 1,
+                name: 'pepperoni', 
+                quantity: 2
+            }
+        }))
+        .toEqual({
+            ...initialState,
+            1: {id: 1, name: 'pepperoni', quantity: 1}
+        })
+    });
+
+    it ('handles PAYMENT_SUCCESS', () => {
+        expect(cartReducer(initialState, actions.paymentSuccess))
         .toEqual({...initialState})
     });
 
